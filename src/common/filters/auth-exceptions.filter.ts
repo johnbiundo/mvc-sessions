c6283d4bc99de20e15d696c098389f5404a9c475
John Biundo <johnfbiundo@gmail.com>
2020-03-06 16:11:36 -0800

    update to latest Nest; fix type bug with flash

--- src/common/filters/auth-exceptions.filter.ts
+++ src/common/filters/auth-exceptions.filter.ts
@@ -9,12 +9,16 @@ import {
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+interface IRequestFlash extends Request {
+  flash: any;
+}
+
 @Catch(HttpException)
 export class AuthExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const request = ctx.getRequest<IRequestFlash>();
 
     if (
       exception instanceof UnauthorizedException ||
